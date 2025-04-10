980d2207ffd29da28639ff0e513fa50d1fb5bdea
prasanth-raj-sankar <prasanthraj0910@gmail.com>
2025-04-10 17:10:09 +0530

    new

--- app.js
+++ app.js
@@ -62,8 +62,8 @@ const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${p
 // MongoDB connection
 mongoose
   .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to MongoDB");
